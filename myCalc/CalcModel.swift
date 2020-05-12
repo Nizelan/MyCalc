054b7dbcb80f507d0a535893a813dd212b7186b3
Nizelan <saintnizelan@gmail.com>
2020-05-12 10:38:05 -0700

    dead comit yet

--- myCalc/CalcModel.swift
+++ myCalc/CalcModel.swift
@@ -11,40 +11,31 @@ import UIKit
 class CalcModel {
     
     public func resolve(string: String) -> Double {
-        var string = string
         var arrayOfString = [String]()
         let operationPriority: [[MathematicalOperation]] =
-            [[.power, .power], [.divide, .multiply], [.add, .substract]]
+            [[.power], [.divide, .multiply], [.add, .substract]]
         
-        while string != "" {
-            var varible: String = ""
-            var char: String?
-            
-            for character in string {
-                if MathematicalOperation(rawValue: String(character)) == nil && char == nil {
-                    varible += String(character)
-                    string.removeFirst(1)
-                } else if MathematicalOperation(rawValue: String(character)) != nil && char == nil{
-                    char = String(character)
-                    string.removeFirst(1)
-                } else if MathematicalOperation(rawValue: String(character)) == nil && char != nil {
-                    break
-                }
-            }
-            arrayOfString.append(varible)
-            guard let action = char else { break }
-            arrayOfString.append(action)
+        arrayOfString = stringAsArray(string: string)
+        
+        if arrayOfString.contains("(") {
+            let i = arrayOfString.firstIndex(of: "(")!
+            let y = arrayOfString.lastIndex(of: ")")!
+            arrayOfString.replaceSubrange(i...y, with: findUrHalf(array: arrayOfString))
         }
         
-        for x in 0..<operationPriority.count {
-            while arrayOfString.contains(where: { $0 == operationPriority[x][0].rawValue || $0 == operationPriority[x][1].rawValue}) {
-                guard let i = arrayOfString.firstIndex(where: { $0 == operationPriority[x][0].rawValue || $0 == operationPriority[x][1].rawValue }) else { break }
-                if let operation = MathematicalOperation(rawValue: arrayOfString[i]) {
-                    let result = calculate(operand: Double(arrayOfString[i-1])!, operand2: Double(arrayOfString[i+1])!, operation: operation)
-                    arrayOfString.replaceSubrange(i-1...i+1, with: repeatElement(String(result), count: 1))
+        for operations in operationPriority {
+            let isStringAnOperationFromCurrentTear: ((String) -> Bool) = { str in
+                if let operation = MathematicalOperation(rawValue: str) {
+                    return operations.contains(operation)
+                } else {
+                    return false
                 }
             }
-            
+            while let i = arrayOfString.firstIndex(where: isStringAnOperationFromCurrentTear) {
+                let operation = MathematicalOperation(rawValue: arrayOfString[i])!
+                let result = calculate(operand: Double(arrayOfString[i-1])!, operand2: Double(arrayOfString[i+1])!, operation: operation)
+                arrayOfString.replaceSubrange(i-1...i+1, with: [String(result)])
+            }
         }
         return Double(arrayOfString[0])!
     }
@@ -62,6 +53,54 @@ class CalcModel {
         case .power:
             return pow(operand, operand2)
         }
+    }
+    
+    func stringAsArray(string: String) -> Array<String> {
+        var string = string
+        var array = [String]()
+        
+        while string != "" {
+            var varible: String = ""
+            var char: String?
+            
+            for character in string {
+                if MathematicalOperation(rawValue: String(character)) == nil && char == nil {
+                    varible += String(character)
+                    string.removeFirst(1)
+                } else if MathematicalOperation(rawValue: String(character)) != nil && char == nil{
+                    char = String(character)
+                    string.removeFirst(1)
+                } else if MathematicalOperation(rawValue: String(character)) == nil && char != nil {
+                    break
+                }
+            }
+            array.append(varible)
+            guard let action = char else { break }
+            array.append(action)
+        }
+        return array
+    }
+    
+    func findUrHalf(array: Array<String>) -> String {
+        var openBracket = 0
+        var clouseBracket = 0
+        var elementsInBrack: String = ""
+        
+        for i in 0..<array.count {
+            if array[i] == "(" {
+                openBracket += 1
+            }
+            if openBracket > clouseBracket && array[i] != "(" {
+                elementsInBrack += array[i]
+            }
+            if array[i] == ")" {
+                clouseBracket += 1
+            }
+            if openBracket == clouseBracket {
+                return String(resolve(string: elementsInBrack))
+            }
+        }
+        return elementsInBrack
     }
 }
 
